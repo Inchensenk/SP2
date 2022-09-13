81f392b1d17334a2168903dfdbdc2c857419dcde
sofoyan gevork <aab81@bk.ru>
2022-09-13 20:04:33 +0300

    Добавьте файлы проекта.

---
+++ SP2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Объявляем объект класса Process
+        Process process = new Process();
+
+        //устанавливаем имя файла, который будет запущен в рамках процесса
+        process.StartInfo.FileName = "notepad.exe";
+
+        //запускаем процесс
+        process.Start();
+
+        //выводим имя процесса
+        Console.WriteLine("Запущен процесс: " + process.ProcessName);
+
+        //ожидаем закрытия процесса
+        process.WaitForExit();
+
+        //выводим код, с которым завершился процесс
+        Console.WriteLine("Процесс завершился с кодом: " + process.ExitCode);
+
+        //выводим имя текущего процесса
+        Console.WriteLine("Текущий процесс имеет имя: " + Process.GetCurrentProcess().ProcessName);
+    }
+}
